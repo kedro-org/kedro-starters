9024b2b23b59fd2d3d4a1315e9e3f99bdc4b65bb
Ivan Danov <idanov@users.noreply.github.com>
2021-01-11 15:09:13 +0000

    Name the nodes in pandas-iris (#10)

--- pandas-iris/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/pipelines/data_science/pipeline.py
+++ pandas-iris/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/pipelines/data_science/pipeline.py
@@ -44,12 +44,18 @@ def create_pipeline(**kwargs):
                 train_model,
                 ["example_train_x", "example_train_y", "parameters"],
                 "example_model",
+                name="train"
             ),
             node(
                 predict,
                 dict(model="example_model", test_x="example_test_x"),
                 "example_predictions",
+                name="predict"
             ),
-            node(report_accuracy, ["example_predictions", "example_test_y"], None),
+            node(
+                report_accuracy,
+                ["example_predictions", "example_test_y"],
+                None,
+                name="report"),
         ]
     )
