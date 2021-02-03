50a2862d68b557df62aed273f3a46a647d0dc23f
Lim Hoang <limdauto@gmail.com>
2021-02-03 21:21:33 +0000

    Update register_config_loader signature

--- pyspark/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/hooks.py
+++ pyspark/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/hooks.py
@@ -48,7 +48,9 @@ class ProjectHooks:
         return {"__default__": Pipeline([])}
 
     @hook_impl
-    def register_config_loader(self, conf_paths: Iterable[str]) -> ConfigLoader:
+    def register_config_loader(
+        self, conf_paths: Iterable[str], env: str, extra_params: Dict[str, Any]
+    ) -> ConfigLoader:
         return ConfigLoader(conf_paths)
 
     @hook_impl
