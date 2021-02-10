a6068c7ecc724f7c951c8016e80b7151a583e167
Andrii Ivaniuk <andrii.ivaniuk@stem.com>
2021-02-10 16:36:03 +0200

    [KED-2357] Removed package name prompt from mini-kedro

--- mini-kedro/hooks/pre_gen_project.py
+++ mini-kedro/hooks/pre_gen_project.py
@@ -42,17 +42,3 @@ if not re.match(r"^\w+(-*\w+)*$", repo_name):
           f"end with alphanumeric symbol.", fg="red", err=True)
     # exits with status 1 to indicate failure
     sys.exit(1)
-
-
-pkg_name = "{{ cookiecutter.python_package }}"
-base_message = f"`{pkg_name}` is not a valid Python package name."
-
-if not re.match(r"^[a-zA-Z_]", pkg_name):
-    secho(base_message + " It must start with a letter or underscore.", fg="red", err=True)
-    sys.exit(1)
-if len(pkg_name) < 2:
-    secho(base_message + " It must be at least 2 characters long.", fg="red", err=True)
-    sys.exit(1)
-if not re.match(r"^\w+$", pkg_name[1:]):
-    secho(base_message + " It must contain only letters, digits, and/or underscores.", fg="red", err=True)
-    sys.exit(1)
