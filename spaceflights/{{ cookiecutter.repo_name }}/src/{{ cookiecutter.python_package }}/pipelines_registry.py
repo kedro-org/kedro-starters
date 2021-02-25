3a7a04d526b4605a7605ccdc56bf532daebae48e
lorenabalan <lorena.balan@quantumblack.com>
2021-02-25 10:41:00 +0000

    Add pipelines_registry.py and remove register_pipelines hook

--- spaceflights/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/pipelines_registry.py
+++ spaceflights/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/pipelines_registry.py
@@ -26,29 +26,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Project hooks."""
-from typing import Any, Dict, Iterable, Optional
+"""Project pipelines."""
+from typing import Dict
 
-from kedro.config import ConfigLoader
-from kedro.framework.hooks import hook_impl
-from kedro.io import DataCatalog
-from kedro.versioning import Journal
+from kedro.pipeline import Pipeline
+
+from {{cookiecutter.python_package}}.pipelines import data_engineering as de
+from {{cookiecutter.python_package}}.pipelines import data_science as ds
 
 
-class ProjectHooks:
-    @hook_impl
-    def register_config_loader(self, conf_paths: Iterable[str]) -> ConfigLoader:
-        return ConfigLoader(conf_paths)
+def register_pipelines() -> Dict[str, Pipeline]:
+    """Register the project's pipeline.
 
-    @hook_impl
-    def register_catalog(
-        self,
-        catalog: Optional[Dict[str, Dict[str, Any]]],
-        credentials: Dict[str, Dict[str, Any]],
-        load_versions: Dict[str, str],
-        save_version: str,
-        journal: Journal,
-    ) -> DataCatalog:
-        return DataCatalog.from_config(
-            catalog, credentials, load_versions, save_version, journal
-        )
+    Returns:
+        A mapping from a pipeline name to a ``Pipeline`` object.
+
+    """
+    data_engineering_pipeline = de.create_pipeline()
+    data_science_pipeline = ds.create_pipeline()
+
+    return {
+        "__default__": data_engineering_pipeline + data_science_pipeline,
+        "de": data_engineering_pipeline,
+        "ds": data_science_pipeline,
+    }
