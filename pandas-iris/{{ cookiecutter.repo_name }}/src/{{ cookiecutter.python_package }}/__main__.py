1b5784dd74267d7b7f761729d800c364b7664d83
Lim Hoang <limdauto@gmail.com>
2021-03-15 14:33:07 +0000

    Update __main__.py to be compatible with 0.17.2

--- pandas-iris/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/__main__.py
+++ pandas-iris/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/__main__.py
@@ -25,20 +25,20 @@
 #
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""Entry point for running a Kedro pipeline as a Python package."""
+"""{{ cookiecutter.project_name }} file for ensuring the package is executable
+as `{{ cookiecutter.repo_name }}` and `python -m {{ cookiecutter.python_package }}`
+"""
 from pathlib import Path
 
-from kedro.framework.session import KedroSession
+from kedro.framework.project import configure_project
+
+from .cli import run
 
 
-def run_package():
-    # Entry point for running a Kedro project packaged with `kedro package`
-    # using `python -m <project_package>.run` command.
-    package_name = Path(__file__).resolve().parent.name
-    with KedroSession.create(package_name) as session:
-        session.run()
+def main():
+    configure_project(Path(__file__).parent.name)
+    run()
 
 
 if __name__ == "__main__":
-    run_package()
+    main()
