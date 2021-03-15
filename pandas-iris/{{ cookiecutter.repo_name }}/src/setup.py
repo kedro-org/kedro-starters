b73b67a99173524650958603049b9fa706e254ed
Lim Hoang <limdauto@gmail.com>
2021-03-15 14:26:40 +0000

    Update setup.py

--- pandas-iris/{{ cookiecutter.repo_name }}/src/setup.py
+++ pandas-iris/{{ cookiecutter.repo_name }}/src/setup.py
@@ -29,7 +29,7 @@
 from setuptools import find_packages, setup
 
 entry_point = (
-    "{{ cookiecutter.repo_name }} = {{ cookiecutter.python_package }}.run:run_package"
+    "{{ cookiecutter.repo_name }} = {{ cookiecutter.python_package }}.__main__:main"
 )
 
 
@@ -52,16 +52,13 @@ setup(
     install_requires=requires,
     extras_require={
         "docs": [
-            "sphinx>=1.6.3, <2.0",
-            "sphinx_rtd_theme==0.4.1",
-            "docutils",
-            "nbsphinx==0.3.4",
+            "sphinx~=3.4.3",
+            "sphinx_rtd_theme==0.5.1",
+            "nbsphinx==0.8.1",
             "nbstripout==0.3.3",
             "recommonmark==0.7.1",
-            "sphinx-autodoc-typehints==1.6.0",
-            "sphinx_copybutton==0.2.5",
-            "jupyter_client>=5.1.0, <7.0",
-            "tornado~=6.1",
+            "sphinx-autodoc-typehints==1.11.1",
+            "sphinx_copybutton==0.3.1",
             "ipykernel~=5.3",
         ]
     },
