db35f885f5c8bf6af1f4f63eab35773292df6758
SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
2022-04-11 13:07:19 +0100

    lint
    Signed-off-by: SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>

--- pandas-iris/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/nodes.py
+++ pandas-iris/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/nodes.py
@@ -10,7 +10,9 @@ import numpy as np
 import pandas as pd
 
 
-def split_data(data: pd.DataFrame, parameters: Dict[str, Any]) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+def split_data(
+    data: pd.DataFrame, parameters: Dict[str, Any]
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
     """Splits data into features and target training and test sets.
 
     Args:
