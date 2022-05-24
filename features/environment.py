55aae5551bf0646d109cee1b3249dac450cb5928
Merel Theisen <merel.theisen@quantumblack.com>
2022-05-24 13:56:57 +0100

    Clean up environment
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- features/environment.py
+++ features/environment.py
@@ -2,7 +2,6 @@
 
 import os
 import shutil
-import stat
 import subprocess
 import tempfile
 import venv
@@ -37,10 +36,6 @@ def before_scenario(context, scenario):
     kedro_install_venv_dir = create_new_venv()
     context.venv_dir = kedro_install_venv_dir
 
-    ## Setup context with venv logic
-    ##context = _setup_context_with_venv(context, kedro_install_venv_dir)
-    ##
-
     if os.name == "posix":
         bin_dir = context.venv_dir / "bin"
     else:
@@ -51,18 +46,18 @@ def before_scenario(context, scenario):
     context.kedro = str(bin_dir / "kedro")
     context.python = str(bin_dir / "python")
 
-    # clone the environment, remove any condas and venvs and insert our venv
-    context.env = os.environ.copy()
-    path = context.env["PATH"].split(os.pathsep)
-    path = [p for p in path if not (Path(p).parent / "pyvenv.cfg").is_file()]
-    path = [p for p in path if not (Path(p).parent / "conda-meta").is_dir()]
-    path = [str(bin_dir)] + path
-    context.env["PATH"] = os.pathsep.join(path)
-
-    # Create an empty pip.conf file and point pip to it
-    pip_conf_path = context.venv_dir / "pip.conf"
-    pip_conf_path.touch()
-    context.env["PIP_CONFIG_FILE"] = str(pip_conf_path)
+    # # clone the environment, remove any condas and venvs and insert our venv
+    # context.env = os.environ.copy()
+    # path = context.env["PATH"].split(os.pathsep)
+    # path = [p for p in path if not (Path(p).parent / "pyvenv.cfg").is_file()]
+    # path = [p for p in path if not (Path(p).parent / "conda-meta").is_dir()]
+    # path = [str(bin_dir)] + path
+    # context.env["PATH"] = os.pathsep.join(path)
+    #
+    # # Create an empty pip.conf file and point pip to it
+    # pip_conf_path = context.venv_dir / "pip.conf"
+    # pip_conf_path.touch()
+    # context.env["PIP_CONFIG_FILE"] = str(pip_conf_path)
 
     starters_root = Path(__file__).parents[1]
     starter_names = [
@@ -76,24 +71,12 @@ def before_scenario(context, scenario):
         starter: str(starters_root / starter) for starter in starter_names
     }
     context.starters_paths = starters_paths
-    subprocess.run([context.pip, "install", "-r", "test_requirements.txt"], env=context.env)
+    subprocess.run([context.pip, "install", "-r", "test_requirements.txt"])
     context.temp_dir = Path(tempfile.mkdtemp()).resolve()
     _PATHS_TO_REMOVE.add(context.temp_dir)
 
-
-# def after_scenario(context, scenario):
-#     rmtree(str(context.temp_dir))
-#     rmtree(str(context.venv_dir))
 
 def after_scenario(context, scenario):
     for path in _PATHS_TO_REMOVE:
         # ignore errors when attempting to remove already removed directories
         shutil.rmtree(path, ignore_errors=True)
-
-
-def rmtree(top):
-    if os.name != "posix":
-        for root, _, files in os.walk(top, topdown=False):
-            for name in files:
-                os.chmod(os.path.join(root, name), stat.S_IWUSR)
-    shutil.rmtree(top)
