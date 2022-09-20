6e7b56add7bde4d6abff184892bc83841c633bc3
Nok <nok.lam.chan@quantumblack.com>
2022-09-20 11:21:00 +0100

    Make use of pipeline autodiscovery in all starters (#101) (#104)
    * Autodiscover pipelines in the spaceflights starter
    
    * Reduce docstring length, as in the main repository
    
    * Update pipeline_registry.py
    
    * Update pipeline_registry.py
    
    * Update pipeline_registry.py
    
    * Update pipeline_registry.py
    
    Co-authored-by: Deepyaman Datta <deepyaman.datta@utexas.edu>

--- astro-airflow-iris/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/pipeline_registry.py
+++ astro-airflow-iris/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/pipeline_registry.py
@@ -1,24 +1,16 @@
 """Project pipelines."""
 from typing import Dict
 
+from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
-
-from {{cookiecutter.python_package}}.pipelines import data_engineering as de
-from {{cookiecutter.python_package}}.pipelines import data_science as ds
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
 
     Returns:
-        A mapping from a pipeline name to a ``Pipeline`` object.
-
+        A mapping from pipeline names to ``Pipeline`` objects.
     """
-    data_engineering_pipeline = de.create_pipeline()
-    data_science_pipeline = ds.create_pipeline()
-
-    return {
-        "de": data_engineering_pipeline,
-        "ds": data_science_pipeline,
-        "__default__": data_engineering_pipeline + data_science_pipeline,
-    }
+    pipelines = find_pipelines()
+    pipelines["__default__"] = sum(pipelines.values())
+    return pipelines
