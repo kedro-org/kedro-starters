db3ba7d7eea6725fc593b492f74cfb426cf5a1c8
Jo Stichbury <stichbury@users.noreply.github.com>
2022-11-10 16:52:06 +0000

    Update spaceflights/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/pipelines/data_processing/pipeline.py
    Co-authored-by: Nok Lam Chan <nok.lam.chan@quantumblack.com>

--- spaceflights/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/pipelines/data_processing/pipeline.py
+++ spaceflights/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/pipelines/data_processing/pipeline.py
@@ -2,9 +2,6 @@ from kedro.pipeline import Pipeline, node, pipeline
 
 from .nodes import create_model_input_table, preprocess_companies, preprocess_shuttles
 
-def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
-
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
