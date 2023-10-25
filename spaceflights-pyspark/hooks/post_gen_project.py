1329d39f4a5671916df22c8054f7fd2464fdea89
SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
2023-10-25 11:02:37 +0100

    Update post_gen_project.py
    Signed-off-by: SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>

--- spaceflights-pyspark/hooks/post_gen_project.py
+++ spaceflights-pyspark/hooks/post_gen_project.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     # Get the selected add-ons from cookiecutter
     selected_add_ons = "{{ cookiecutter.add_ons }}"
     # Parse the add-ons to get a list
-    selected_add_ons_list = parse_add_ons_input(selected_add_ons)
+    selected_add_ons_list = _parse_add_ons_input(selected_add_ons)
 
     # Execute the script only if the PySpark add-on (represented by "6") is selected.
     # This ensures the script doesn't run with kedro new --starter but only with the add-ons flow option.
