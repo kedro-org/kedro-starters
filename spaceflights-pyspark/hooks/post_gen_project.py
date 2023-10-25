542cd2689ffc77bb04c6830e345ece96b6b5b799
Sajid Alam <90610031+SajidAlamQB@users.noreply.github.com>
2023-10-25 16:06:14 +0100

    Move `tests` folder to be aligned with `develop` template (#170)
    * Update post_gen_project.py
    
    Signed-off-by: SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
    
    * move tests for spaceflights-pyspark
    
    Signed-off-by: SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
    
    * move all test folders
    
    Signed-off-by: SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
    
    * update comment
    
    Signed-off-by: SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
    
    * Update post_gen_project.py
    
    Signed-off-by: SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>

--- spaceflights-pyspark/hooks/post_gen_project.py
+++ spaceflights-pyspark/hooks/post_gen_project.py
@@ -8,14 +8,14 @@ from kedro.templates.project.hooks.utils import (
 from kedro.framework.cli.starters import _parse_add_ons_input
 
 
-def main(selected_add_ons_list):
+def main(selected_add_ons):
     current_dir = Path.cwd()
     requirements_file_path = current_dir / "requirements.txt"
     pyproject_file_path = current_dir / "pyproject.toml"
     python_package_name = '{{ cookiecutter.python_package }}'
 
     # Handle template directories and requirements according to selected add-ons
-    setup_template_add_ons(selected_add_ons_list, requirements_file_path, pyproject_file_path, python_package_name)
+    setup_template_add_ons(selected_add_ons, requirements_file_path, pyproject_file_path, python_package_name)
 
     # Sort requirements.txt file in alphabetical order
     sort_requirements(requirements_file_path)
@@ -24,10 +24,8 @@ def main(selected_add_ons_list):
 if __name__ == "__main__":
     # Get the selected add-ons from cookiecutter
     selected_add_ons = "{{ cookiecutter.add_ons }}"
-    # Parse the add-ons to get a list
-    selected_add_ons_list = _parse_add_ons_input(selected_add_ons)
 
-    # Execute the script only if the PySpark add-on (represented by "6") is selected.
+    # Execute the script only if the Pyspark add-on is selected.
     # This ensures the script doesn't run with kedro new --starter but only with the add-ons flow option.
-    if "6" in selected_add_ons_list:
-        main(selected_add_ons_list)
+    if "Pyspark" in selected_add_ons:
+        main(selected_add_ons)
