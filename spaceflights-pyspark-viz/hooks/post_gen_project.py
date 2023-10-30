f8be5045654555e100b1c0b43fd53059f8beade8
Nok Lam Chan <nok.lam.chan@quantumblack.com>
2023-10-30 16:07:48 +0000

    Support post_gen hook for viz and pyspark-viz starter (#172)
    * Support post_gen hook for viz and pyspark-viz starter
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * fix pandas-viz starteR
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    
    * Update spaceflights-pandas-viz/hooks/post_gen_project.py
    
    Co-authored-by: Sajid Alam <90610031+SajidAlamQB@users.noreply.github.com>
    
    * update cookiecutter.json
    
    Signed-off-by: SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>
    Signed-off-by: SajidAlamQB <90610031+SajidAlamQB@users.noreply.github.com>
    Co-authored-by: Sajid Alam <90610031+SajidAlamQB@users.noreply.github.com>

---
+++ spaceflights-pyspark-viz/hooks/post_gen_project.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from kedro.templates.project.hooks.utils import (
+
+    setup_template_add_ons,
+    sort_requirements,
+)
+from kedro.framework.cli.starters import _parse_add_ons_input
+
+
+def main(selected_add_ons):
+    current_dir = Path.cwd()
+    requirements_file_path = current_dir / "requirements.txt"
+    pyproject_file_path = current_dir / "pyproject.toml"
+    python_package_name = '{{ cookiecutter.python_package }}'
+
+    # Handle template directories and requirements according to selected add-ons
+    setup_template_add_ons(selected_add_ons, requirements_file_path, pyproject_file_path, python_package_name)
+
+    # Sort requirements.txt file in alphabetical order
+    sort_requirements(requirements_file_path)
+
+
+if __name__ == "__main__":
+    # Get the selected add-ons from cookiecutter
+    selected_add_ons = "{{ cookiecutter.add_ons }}"
+
+    # Execute the script only if the Pyspark add-on is selected.
+    # This ensures the script doesn't run with kedro new --starter but only with the add-ons flow option.
+    if "Pyspark" in selected_add_ons and "Kedro Viz" in selected_add_ons:
+        main(selected_add_ons)
