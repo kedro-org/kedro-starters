7ae752f02d1a06e2b6e60e659061f968a8e099d6
Dmitry Sorokin <dmd40in@gmail.com>
2023-11-15 16:13:17 +0000

    Add example option to post_gen hooks in 4 starters
    Signed-off-by: Dmitry Sorokin <dmd40in@gmail.com>

--- spaceflights-pyspark/hooks/post_gen_project.py
+++ spaceflights-pyspark/hooks/post_gen_project.py
@@ -13,9 +13,10 @@ def main(selected_add_ons):
     requirements_file_path = current_dir / "requirements.txt"
     pyproject_file_path = current_dir / "pyproject.toml"
     python_package_name = '{{ cookiecutter.python_package }}'
+    example_pipeline = "{{ cookiecutter.example_pipeline }}"
 
     # Handle template directories and requirements according to selected add-ons
-    setup_template_add_ons(selected_add_ons, requirements_file_path, pyproject_file_path, python_package_name)
+    setup_template_add_ons(selected_add_ons, requirements_file_path, pyproject_file_path, python_package_name, example_pipeline)
 
     # Sort requirements.txt file in alphabetical order
     sort_requirements(requirements_file_path)
