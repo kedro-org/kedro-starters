5029e57215045b991c8e02d00222367c87f3ffd8
Merel Theisen <49397448+merelcht@users.noreply.github.com>
2023-11-22 16:05:21 +0000

    Explicitly set `base_env` and `default_run_env` in `settings.py` (#186)
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- spaceflights-pyspark-viz/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/settings.py
+++ spaceflights-pyspark-viz/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/settings.py
@@ -30,8 +30,8 @@ from kedro.config import OmegaConfigLoader  # noqa: E402
 CONFIG_LOADER_CLASS = OmegaConfigLoader
 # Keyword arguments to pass to the `CONFIG_LOADER_CLASS` constructor.
 CONFIG_LOADER_ARGS = {
-#       "base_env": "base",
-#       "default_run_env": "local",
+    "base_env": "base",
+    "default_run_env": "local",
     "config_patterns": {
         "spark": ["spark*", "spark*/**"],
     }
