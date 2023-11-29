4718fade367978b3bf42cd1ad6d7450783ea1de0
Ahdra Merali <90615669+AhdraMeraliQB@users.noreply.github.com>
2023-11-29 17:46:35 +0000

    Rename add-ons to tools (#189)
    * Rename add-ons to tools
    
    Signed-off-by: Ahdra Merali <ahdra.merali@quantumblack.com>
    
    * Retrigger CI
    
    Signed-off-by: Ahdra Merali <ahdra.merali@quantumblack.com>
    
    ---------
    
    Signed-off-by: Ahdra Merali <ahdra.merali@quantumblack.com>

--- spaceflights-pyspark-viz/hooks/post_gen_project.py
+++ spaceflights-pyspark-viz/hooks/post_gen_project.py
@@ -2,30 +2,30 @@ from pathlib import Path
 
 from kedro.templates.project.hooks.utils import (
 
-    setup_template_add_ons,
+    setup_template_tools,
     sort_requirements,
 )
 
 
-def main(selected_add_ons):
+def main(selected_tools):
     current_dir = Path.cwd()
     requirements_file_path = current_dir / "requirements.txt"
     pyproject_file_path = current_dir / "pyproject.toml"
     python_package_name = '{{ cookiecutter.python_package }}'
     example_pipeline = "{{ cookiecutter.example_pipeline }}"
 
-    # Handle template directories and requirements according to selected add-ons
-    setup_template_add_ons(selected_add_ons, requirements_file_path, pyproject_file_path, python_package_name, example_pipeline)
+    # Handle template directories and requirements according to selected tools
+    setup_template_tools(selected_tools, requirements_file_path, pyproject_file_path, python_package_name, example_pipeline)
 
     # Sort requirements.txt file in alphabetical order
     sort_requirements(requirements_file_path)
 
 
 if __name__ == "__main__":
-    # Get the selected add-ons from cookiecutter
-    selected_add_ons = "{{ cookiecutter.add_ons }}"
+    # Get the selected tools from cookiecutter
+    selected_tools = "{{ cookiecutter.tools }}"
 
-    # Execute the script only if the Pyspark add-on is selected.
-    # This ensures the script doesn't run with kedro new --starter but only with the add-ons flow option.
-    if "Pyspark" in selected_add_ons and "Kedro Viz" in selected_add_ons:
-        main(selected_add_ons)
+    # Execute the script only if the Pyspark tool is selected.
+    # This ensures the script doesn't run with kedro new --starter but only with the tools flow option.
+    if "Pyspark" in selected_tools and "Kedro Viz" in selected_tools:
+        main(selected_tools)
