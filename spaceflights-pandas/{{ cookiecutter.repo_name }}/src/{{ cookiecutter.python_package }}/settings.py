aa8ed2666ae0a68cc8752c9aedd66b939687154a
ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>
2024-08-05 13:50:44 -0500

    fix lint

--- spaceflights-pandas/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/settings.py
+++ spaceflights-pandas/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/settings.py
@@ -17,9 +17,10 @@ https://docs.kedro.org/en/stable/kedro_project_setup/settings.html."""
 # SESSION_STORE_CLASS = BaseSessionStore
 
 # Setup for Experiment Tracking
-# The SQLite DB required for experiment tracking is stored by default in the .viz folder (supported from python >= 3.9 and Kedro-Viz 9.2.0) 
-# of your project. To store it in another directory, provide the keyword argument `SESSION_STORE_ARGS`
-# to pass to the `SESSION_STORE_CLASS` constructor.
+# The SQLite DB required for experiment tracking is stored by default
+# (supported from python >= 3.9 and Kedro-Viz 9.2.0) in the .viz folder
+# of your project. To store it in another directory, provide the keyword argument
+# `SESSION_STORE_ARGS` to pass to the `SESSION_STORE_CLASS` constructor.
 # SESSION_STORE_ARGS = {"path": str(Path(__file__).parents[2] / ".viz")}
 
 # Directory that holds configuration.
--- spaceflights-pyspark-viz/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/settings.py
+++ spaceflights-pyspark-viz/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/settings.py
@@ -13,14 +13,16 @@ HOOKS = (SparkHooks(),)
 
 # Class that manages storing KedroSession data.
 from pathlib import Path  # noqa: E402
+
 from kedro_viz.integrations.kedro.sqlite_store import SQLiteStore  # noqa: E402
 
 SESSION_STORE_CLASS = SQLiteStore
 
 # Setup for Experiment Tracking
-# The SQLite DB required for experiment tracking is stored by default in the .viz folder (supported from python >= 3.9 and Kedro-Viz 9.2.0) 
-# of your project. To store it in another directory, provide the keyword argument `SESSION_STORE_ARGS`
-# to pass to the `SESSION_STORE_CLASS` constructor.
+# The SQLite DB required for experiment tracking is stored by default
+# (supported from python >= 3.9 and Kedro-Viz 9.2.0) in the .viz folder
+# of your project. To store it in another directory, provide the keyword argument
+# `SESSION_STORE_ARGS` to pass to the `SESSION_STORE_CLASS` constructor.
 SESSION_STORE_ARGS = {"path": str(Path(__file__).parents[2] / ".viz")}
 
 # Directory that holds configuration.
