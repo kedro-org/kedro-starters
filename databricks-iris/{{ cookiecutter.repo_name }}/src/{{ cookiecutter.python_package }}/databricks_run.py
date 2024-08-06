47ab516f9ec9a2f40d6aeab16bbbdb2048bcad5d
Merel Theisen <merel.theisen@quantumblack.com>
2024-08-06 10:39:31 +0100

    Revert changes for databricks iris
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

---
+++ databricks-iris/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/databricks_run.py
@@ -0,0 +1,34 @@
+import argparse
+import logging
+
+from kedro.framework.project import configure_project
+from kedro.framework.session import KedroSession
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", dest="env", type=str)
+    parser.add_argument("--conf-source", dest="conf_source", type=str)
+    parser.add_argument("--package-name", dest="package_name", type=str)
+    parser.add_argument("--nodes", dest="nodes", type=str)
+
+    args = parser.parse_args()
+    env = args.env
+    conf_source = args.conf_source
+    package_name = args.package_name
+    nodes = [node.strip() for node in args.nodes.split(",")]
+
+    # https://kb.databricks.com/notebooks/cmd-c-on-object-id-p0.html
+    logging.getLogger("py4j.java_gateway").setLevel(logging.ERROR)
+    logging.getLogger("py4j.py4j.clientserver").setLevel(logging.ERROR)
+
+    configure_project(package_name)
+    with KedroSession.create(env=env, conf_source=conf_source) as session:
+        if not nodes:
+            session.run()
+        else:
+            session.run(node_names=nodes)
+
+
+if __name__ == "__main__":
+    main()
