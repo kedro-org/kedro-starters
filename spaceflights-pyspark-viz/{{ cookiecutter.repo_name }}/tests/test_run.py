6db3e60e062260311e2fd51d8fa51a0af0e7a8e3
Laura Couto <laurarccouto@gmail.com>
2024-12-04 18:35:32 -0300

    Simplify session.run() test
    Signed-off-by: Laura Couto <laurarccouto@gmail.com>

--- spaceflights-pyspark-viz/{{ cookiecutter.repo_name }}/tests/test_run.py
+++ spaceflights-pyspark-viz/{{ cookiecutter.repo_name }}/tests/test_run.py
@@ -7,45 +7,14 @@ from pathlib import Path
 
 from kedro.framework.session import KedroSession
 from kedro.framework.startup import bootstrap_project
-from kedro.io import DataCatalog
 
 # The tests below are here for the demonstration purpose
 # and should be replaced with the ones testing the project
 # functionality
 
-class TestKedroSession:
-    def test_config_loader(self):
+class TestKedroRun:
+    def test_kedro_run(self):
         bootstrap_project(Path.cwd())
 
         with KedroSession.create(project_path=Path.cwd()) as session:
-            # Test creating a Kedro session and verifying the config loader
-            context = session.load_context()
-            config_loader = context.config_loader
-            assert str(config_loader.conf_source) == str(Path.cwd() / "conf")
-
-            base_params = config_loader.get("parameters", "base")
-            assert base_params == {}
-
-    def test_data_catalog(self):
-        bootstrap_project(Path.cwd())
-
-        # Test creating a Kedro session and verifying its data catalog
-        with KedroSession.create(project_path=Path.cwd()) as session:
-            context = session.load_context()
-            catalog = context.catalog
-            assert isinstance(catalog, DataCatalog)
-
-            # Test if parameters can be loaded
-            parameters = catalog.load("parameters")
-            assert isinstance(parameters, dict)
-
-    def test_default_runner(self):
-        bootstrap_project(Path.cwd())
-        with KedroSession.create(project_path=Path.cwd()) as session:
-            # Test if the runner exists
-            assert session.run is not None
-
-    def test_project_path(self):
-        bootstrap_project(Path.cwd())
-        with KedroSession.create(project_path=Path.cwd()) as session:
-            assert session._project_path == Path.cwd()
+            assert session.run() is not None
