2d7346fe67066676d56f997d4becb181b1196404
Laura Couto <laurarccouto@gmail.com>
2025-07-04 13:48:04 -0300

    Lint
    Signed-off-by: Laura Couto <laurarccouto@gmail.com>

--- spaceflights-pandas/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/pipelines/reporting/nodes.py
+++ spaceflights-pandas/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/pipelines/reporting/nodes.py
@@ -37,19 +37,19 @@ def compare_passenger_capacity_go(preprocessed_shuttles: pd.DataFrame):
 
 def create_confusion_matrix(companies: pd.DataFrame):
     matplotlib.use('Agg')
-    
+
     actuals = [0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1]
     predicted = [1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1]
     data = {"y_Actual": actuals, "y_Predicted": predicted}
     df = pd.DataFrame(data, columns=["y_Actual", "y_Predicted"])
-    
+
     confusion_matrix = pd.crosstab(
         df["y_Actual"], df["y_Predicted"], rownames=["Actual"], colnames=["Predicted"]
     )
-    
+
     fig, ax = plt.subplots(figsize=(8, 6))
     sn.heatmap(confusion_matrix, annot=True, fmt='d', cmap='Blues', ax=ax)
     ax.set_title('Confusion Matrix')
     plt.tight_layout()
-    
+
     return fig
