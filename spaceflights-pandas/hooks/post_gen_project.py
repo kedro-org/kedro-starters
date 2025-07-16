36421aa1bf76d69ef5d3fec828cd5aeba32ad36b
Ankita Katiyar <110245118+ankatiyar@users.noreply.github.com>
2025-07-16 15:43:01 +0100

    Remove sort_requirements (#289)
    Signed-off-by: Ankita Katiyar <ankitakatiyar2401@gmail.com>

--- spaceflights-pandas/hooks/post_gen_project.py
+++ spaceflights-pandas/hooks/post_gen_project.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from kedro.templates.project.hooks.utils import (
 
     setup_template_tools,
-    sort_requirements,
 )
 
 
@@ -17,8 +16,6 @@ def main(example_pipeline):
     # Handle template directories and requirements according to selected tools
     setup_template_tools(selected_tools, requirements_file_path, pyproject_file_path, python_package_name, example_pipeline)
 
-    # Sort requirements.txt file in alphabetical order
-    sort_requirements(requirements_file_path)
 
 
 if __name__ == "__main__":
