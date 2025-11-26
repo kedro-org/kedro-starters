974a374d98de0835715c586d7cba1e77df76b38e
L. R. Couto <57910428+lrcouto@users.noreply.github.com>
2025-11-26 10:01:29 -0300

    Fix wrongful None node output (#299)
    Signed-off-by: Laura Couto <laurarccouto@gmail.com>

--- spaceflights-pyspark/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/pipelines/data_science/pipeline.py
+++ spaceflights-pyspark/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/pipelines/data_science/pipeline.py
@@ -21,7 +21,7 @@ def create_pipeline(**kwargs) -> Pipeline:
             Node(
                 func=evaluate_model,
                 inputs=["regressor", "X_test", "y_test"],
-                outputs=None,
+                outputs="metrics",
                 name="evaluate_model_node",
             ),
         ]
