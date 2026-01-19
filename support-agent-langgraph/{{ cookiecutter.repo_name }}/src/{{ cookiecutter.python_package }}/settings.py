79ff8df2321ce74bbc565772f322cb14ec3ed480
Elena Khaustova <ymax70rus@gmail.com>
2026-01-19 16:22:21 +0000

    Updated comments
    Signed-off-by: Elena Khaustova <ymax70rus@gmail.com>

--- support-agent-langgraph/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/settings.py
+++ support-agent-langgraph/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/settings.py
@@ -47,11 +47,14 @@ CONFIG_LOADER_ARGS = {
 import warnings
 from kedro.utils import KedroExperimentalWarning
 
-# This starter uses multiple experimental features and maked as experimental itself
-# We want this warning appears only once per starter, not per each experimental feature
+# This starter uses multiple experimental features and is marked as experimental itself.
+# We emit a single experimental warning for the whole starter.
+# To re-enable individual experimental warnings from Kedro,
+# REMOVE the `warnings.filterwarnings(...)` call below.
 warnings.warn(
-    f"support-agent-langgraph stater is experimental and may change in future Kedro releases.",
+    "support-agent-langgraph starter is experimental and may change in future Kedro releases.",
     category=KedroExperimentalWarning,
 )
 
+# Suppresses all subsequent KedroExperimentalWarning warnings
 warnings.filterwarnings("ignore", category=KedroExperimentalWarning)
